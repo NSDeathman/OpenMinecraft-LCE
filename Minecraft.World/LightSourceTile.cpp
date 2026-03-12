daef2225bf862594f689883b250a823f6d928a13
NSDeathman <forest.kir.325@gmail.com>
2026-03-12 15:32:18 +0300

    Light block pick fix

--- Minecraft.World/LightSourceTile.cpp
+++ Minecraft.World/LightSourceTile.cpp
@@ -4,25 +4,15 @@
 #include "LightSourceTile.h"
 
 LightSourceTile::LightSourceTile(int id)
-    : Tile(id, Material::air, /*isSolidRender=*/false)
+    : Tile(id, Material::decoration, /*isSolidRender=*/false)
 {
     setLightEmission(1.0f);
     setDestroyTime(0.0f);
 }
 
-AABB* LightSourceTile::getAABB(Level* level, int x, int y, int z)
-{
-    return nullptr; // нет физической коллизии
-}
-
-AABB* LightSourceTile::getTileAABB(Level* level, int x, int y, int z)
-{
-    return nullptr; // нет коллизии дл€ выделени€
-}
-
 bool LightSourceTile::isSolidRender(bool isServerLevel)
 {
-    return false; // не закрывает соседние грани
+    return false;
 }
 
 bool LightSourceTile::isCubeShaped()
@@ -32,10 +22,40 @@ bool LightSourceTile::isCubeShaped()
 
 int LightSourceTile::getRenderShape()
 {
-    return Tile::SHAPE_INVISIBLE; // рендер пропускаетс€
+    return Tile::SHAPE_INVISIBLE;
+}
+
+AABB* LightSourceTile::getAABB(Level* level, int x, int y, int z)
+{
+    return nullptr; // нет физической коллизии
+}
+
+AABB* LightSourceTile::getTileAABB(Level* level, int x, int y, int z)
+{
+    return nullptr; // нет рамки выделени€
+}
+
+HitResult* LightSourceTile::clip(Level* level, int x, int y, int z, Vec3* a, Vec3* b)
+{
+    return nullptr; // луч проходит сквозь блок, прицел не реагирует
+}
+
+float LightSourceTile::getDestroyProgress(shared_ptr<Player> player, Level* level, int x, int y, int z)
+{
+    return 0.0f; // прогресс разрушени€ всегда 0 Ч сломать невозможно
+}
+
+bool LightSourceTile::mayPick()
+{
+    return false; // нельз€ подобрать средней кнопкой
+}
+
+int LightSourceTile::getResourceCount(Random* random)
+{
+    return 0; // не выбрасывает ничего
 }
 
 bool LightSourceTile::isPathfindable(LevelSource* level, int x, int y, int z)
 {
-    return true; // мобы проход€т сквозь блок
+    return true; // мобы проход€т насквозь
 }
